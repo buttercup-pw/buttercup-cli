a2f4f740c6ddcd908929b198a3a32dd6c6f6fb93
Perry Mitchell <perry@perrymitchell.net>
2017-05-11 15:04:46 +0200

    Add 'tree' command for complete output

--- source/logic.js
+++ source/logic.js
@@ -1,4 +1,35 @@
 const { listGroups, selectGroupByIndex, selectPreviousGroup } = require("./buttercup.js");
+const { getSharedWorkspace } = require("./buttercup.js");
+const { colourTitle, colourItemTitle, dimColour } = require("./terminal.js");
+
+const INDENT = "   ";
+
+function displayTree() {
+    const workspace = getSharedWorkspace();
+    const archive = workspace.primary.archive;
+    const getIndentation = (amount) => {
+        let ind = "";
+        while (amount > 0) {
+            ind += INDENT;
+            amount -= 1;
+        }
+        return ind;
+    }
+    const renderTreeNode = (node, level = 0) => {
+        const nodeTitle = node.getTitle ?
+            node.getTitle() :
+            "Archive";
+        const indent = getIndentation(level);
+        console.log(indent + colourTitle(nodeTitle));
+        if (typeof node.getEntries === "function" && node.getEntries().length > 0) {
+            node.getEntries().forEach(function(entry) {
+                console.log(indent + "   " + colourItemTitle(entry.getProperty("title")));
+            });
+        }
+        node.getGroups().forEach(group => renderTreeNode(group, level + 1));
+    };
+    renderTreeNode(archive);
+}
 
 function handleGroupsCommand(commandParts) {
     const primary = commandParts.shift();
@@ -26,6 +57,8 @@ function processCommand(commandParts) {
     switch (primary) {
         case "groups":
             return handleGroupsCommand(commandParts);
+        case "tree":
+            return displayTree();
         case "quit":
             /* falls-through */
         case "exit":
